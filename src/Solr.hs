45fd21ec33bb4c7ee6eb56c36667227f0f966fcf
Alexandre Rademaker <arademaker@gmail.com>
2021-03-02 21:49:27 -0300

    improved code reading synset object

--- src/Solr.hs
+++ src/Solr.hs
@@ -4,56 +4,99 @@ module Solr where
 
 import Data.Aeson
 import qualified Data.ByteString.Lazy as L
--- import qualified Data.ByteString.Lazy.Char8 as C
-import GHC.Generics
 import Data.Either
 import Data.List
+import GHC.Generics
 
+data Pointer =
+  Pointer
+    { target_synset :: String
+    , pointer :: String
+    , target_word :: Maybe String
+    , source_word :: Maybe String
+    , name :: Maybe String
+    }
+  deriving (Show, Generic)
 
-data Pointer = Pointer
-  { target_synset :: String
-  , pointer :: String
-  , target_word :: Maybe String
-  , source_word :: Maybe String
-  , name :: Maybe String
-  } deriving (Show, Generic)
+-- relations and morphosemantic links were extracted from the wn.json with
+-- for s in `egrep -o  "wn30_en_[a-zA-Z]+" wn.json | sort | uniq  | sort`; do echo $s ":: Maybe [Pointer]," ; done
+data Synset =
+  Synset
+    { word_count_pt :: Int
+    , word_count_en :: Int
+    , wn30_synsetId :: [String]
+    , rdf_type :: [String]
+    , gloss_en :: [String]
+    , word_en :: [String]
+    , word_pt :: Maybe [String]
+    , doc_id :: String
+    , wn30_lexicographerFile :: [String]
+    -- relations
+    , wn30_en_adjectivePertainsTo :: Maybe [Pointer]
+    , wn30_en_adverbPertainsTo :: Maybe [Pointer]
+    , wn30_en_antonymOf :: Maybe [Pointer]
+    , wn30_en_attribute :: Maybe [Pointer]
+    , wn30_en_causes :: Maybe [Pointer]
+    , wn30_en_classifiedByRegion :: Maybe [Pointer]
+    , wn30_en_classifiedByTopic :: Maybe [Pointer]
+    , wn30_en_classifiedByUsage :: Maybe [Pointer]
+    , wn30_en_classifiesByRegion :: Maybe [Pointer]
+    , wn30_en_classifiesByTopic :: Maybe [Pointer]
+    , wn30_en_classifiesByUsage :: Maybe [Pointer]
+    , wn30_en_derivationallyRelated :: Maybe [Pointer]
+    , wn30_en_hasInstance :: Maybe [Pointer]
+    , wn30_en_hypernymOf :: Maybe [Pointer]
+    , wn30_en_hyponymOf :: Maybe [Pointer]
+    , wn30_en_instanceOf :: Maybe [Pointer]
+    , wn30_en_memberHolonymOf :: Maybe [Pointer]
+    , wn30_en_memberMeronymOf :: Maybe [Pointer]
+    , wn30_en_partHolonymOf :: Maybe [Pointer]
+    , wn30_en_partMeronymOf :: Maybe [Pointer]
+    , wn30_en_participleOf :: Maybe [Pointer]
+    , wn30_en_sameVerbGroupAs :: Maybe [Pointer]
+    , wn30_en_seeAlso :: Maybe [Pointer]
+    , wn30_en_substanceHolonymOf :: Maybe [Pointer]
+    , wn30_en_substanceMeronymOf :: Maybe [Pointer]
+    -- morphosemantic links
+    , wn30_en_property :: Maybe [Pointer]
+    , wn30_en_result :: Maybe [Pointer]
+    , wn30_en_state :: Maybe [Pointer]
+    , wn30_en_undergoer :: Maybe [Pointer]
+    , wn30_en_uses :: Maybe [Pointer]
+    , wn30_en_vehicle :: Maybe [Pointer]
+    , wn30_en_entails :: Maybe [Pointer]
+    , wn30_en_event :: Maybe [Pointer]
+    , wn30_en_instrument :: Maybe [Pointer]
+    , wn30_en_location :: Maybe [Pointer]
+    , wn30_en_material :: Maybe [Pointer]
+    , wn30_en_agent :: Maybe [Pointer]
+    , wn30_en_bodyPart :: Maybe [Pointer]
+    , wn30_en_byMeansOf :: Maybe [Pointer]
+    , wn30_en_destination :: Maybe [Pointer]
+    }
+  deriving (Show, Generic)
 
-data Synset = Synset {
-  word_count_pt :: Int,
-  word_count_en :: Int,  
-  wn30_synsetId :: [String],
-  rdf_type :: [String],
-  gloss_en :: [String],
-  word_en :: [String],
-  word_pt :: Maybe [String],
-  wn30_lexicographerFile :: [String],
-  wn30_en_classifiedByRegion :: Maybe [Pointer],
-  wn30_en_hypernymOf :: Maybe [Pointer],
-  wn30_en_hyponymOf :: Maybe [Pointer],
-  wn30_en_antonymOf :: Maybe [Pointer],
-  wn30_en_partMeronymOf :: Maybe [Pointer],
-  wn30_en_memberHolonymOf :: Maybe [Pointer],
-  doc_id :: String,
-  wn30_en_instanceOf :: Maybe [Pointer]
-} deriving (Show, Generic)  
+data Document =
+  Document
+    { _index :: String
+    , _type :: String
+    , _id :: String
+    , _score :: Int
+    , _source :: Synset
+    }
+  deriving (Show, Generic)
 
-data Document = Document {
-  _index :: String,
-  _type :: String,
-  _id :: String,
-  _score :: Int,
-  _source :: Synset
-} deriving (Show, Generic)
+customOps = defaultOptions {rejectUnknownFields = True}
+
+instance FromJSON Synset where
+  parseJSON = genericParseJSON customOps
 
 instance FromJSON Pointer
-instance FromJSON Synset
+
 instance FromJSON Document
 
-
 readJ :: L.ByteString -> Either String Document
-readJ s = do
-  doc <- eitherDecode s :: Either String Document
-  return doc
+readJ s = eitherDecode s :: Either String Document
 
 readJL :: FilePath -> IO [Either String Document]
 readJL path = do
