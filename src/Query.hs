c10034d9355962a6d3f823558c4ded3617305c64
FredsoNerd <fredsonerd@gmail.com>
2021-03-07 00:37:08 -0300

    query for senses

--- src/Query.hs
+++ src/Query.hs
@@ -3,11 +3,13 @@
 module Query where
 
 import Solr
-import Data.Maybe
+import Update
 import Data.List
+import Data.Char
+import Data.Maybe
 
 {-
-The attemp is to group elements of form (?a,?ta,?b,?b,?rel) given synsets
+The attemp is to group elements of form (?a,?ta,?b,?tb,?rel) given synsets
 from portuguese related to synsets in english that sustain a relation ?rel
 
  - ?a and ?b are word/lexicalform
@@ -17,40 +19,41 @@ from portuguese related to synsets in english that sustain a relation ?rel
 see: github.com/NLP-CISUC/PT-LexicalSemantics/blob/master/OWN-PT/query.sparql
 -}
 
-query :: [Synset] -> [(String,String,String,String,String)]
-query synsets =
-  [(a,ta,b,tb,rel) | (syna,rel,synb) <- collectRelationSynsets synsets
-                   , a <- fromMaybe [] (word_pt syna)
-                   , ta <- (rdf_type syna)
-                   , b <- fromMaybe [] (word_pt synb)
-                   , tb <- (rdf_type synb)]
-  
 
-collectRelationSynsets :: [Synset] -> [(Synset, String, Synset)]
-collectRelationSynsets synsets =
-  collectSynsets [] (sortBy cRelIds relationIds) (sort synsets)
+collectRelationsSenses :: [Synset] -> [(Sense, RDFType, Sense, RDFType, Relation)]
+collectRelationsSenses synsets =
+  [ (map toLower a, ta, map toLower b, tb, pointer p)
+  | (synA,p,synB) <- collectPointersSynsets synsets
+  , a <- choseSenseWords synA (target_word p)
+  , b <- choseSenseWords synB (target_word p)
+  , ta <- rdf_type synA
+  , tb <- rdf_type synB]
   where
-    trd (_,_,x) = x
-    cRelIds x y = compare (trd x) (trd y)
-    relationIds = collectRelationIds synsets
-    collectSynsets out [] _ = out
-    -- collectSynsets out _ [] = out
-    collectSynsets out ((syn,rel,id):rids) (sy:sys) =
-      case (compare id (doc_id sy)) of
-        GT -> collectSynsets out ((syn,rel,id):rids) sys
-        EQ -> collectSynsets ((syn,rel,sy):out) rids (sy:sys)
-        -- LT -> collectSynsets out rids (sy:sys)
-    
-collectRelationIds :: [Synset] -> [(Synset, String, String)]
-collectRelationIds synsets =
-  [(sy, pointer pt, target_synset pt) | sy <- synsets
-                                      , pt <- collectPointers sy]
+    choseSenseWords synX Nothing = fromMaybe [] (word_pt synX)
+    choseSenseWords synX word = [fromJust word]
+  
+  
+collectPointersSynsets :: [Synset] -> [(Synset, Pointer, Synset)]
+collectPointersSynsets synsets =
+  collectSynsets [] (sortBy compareIds relationIds) (sort synsets)
+  where
+    compareIds x y = compare (target_synset $ snd x) (target_synset $ snd y)
+    relationIds = [(s,p) | s <- synsets, p <- collectPointers s]
+
+
+collectSynsets out [] _ = out
+collectSynsets out (rid:rids) (syn:syns) =
+  case (compare (target_synset $ snd rid) (doc_id syn)) of
+    GT -> collectSynsets out (rid:rids) syns
+    EQ -> collectSynsets ((fst rid, snd rid, syn):out) rids (syn:syns)
+
 
 collectPointers :: Synset -> [Pointer]
 collectPointers synset =
-  concat . (map fromJust) . (filter isJust) $ pointers
+  concat $ map (fromMaybe []) $ pointers
   where
     pointers = [relation synset | relation <- relations]
+
 
 -- relation fuctions
 relations :: [Synset -> Maybe [Pointer]]
