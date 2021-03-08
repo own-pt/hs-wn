23052980ba23938aa69d63f6a0f2edf94fc1be73
FredsoNerd <fredsonerd@gmail.com>
2021-03-08 00:09:01 -0300

    cli working

--- app/Main.hs
+++ app/Main.hs
@@ -4,25 +4,65 @@ import Solr
 import Query
 import Filter
 import Update
+
 import Data.List
 
-main :: IO ()
-main = do
-  out <- showOut <$> process 20
-  putStrLn out
-  where
-    showOut [] = ""
-    showOut ((rel,_):rels) = (wordA rel) ++"\t"++ (intercalate "/" $ wordB rel) ++ "\n" ++ (showOut rels)
+import Control.Monad
 
-process trashold  =
-  sortBy (\x y -> compare (snd y) (snd x)) <$> (frequencyFilter trashold <$> relationsSyns <*> filteredFreq)
-relationsSyns = collectRelationsSenses <$> filteredSyns
-filteredFreq = (parseFrequencies) <$> (getFrequencies "/home/fredson/wn/dhbb/frequencies")
-filteredSyns =
-  c4 <$> sy_doc <*> sg_filter
+import System.Exit
+import System.Directory
+import System.Environment
+
+main :: IO ()
+main = getArgs >>= parse
+
+{- deals with CLI -}
+parse ["-h"] = usage >> exitSuccess
+parse input  = run input >> exitSuccess
+
+usage = putStrLn "usage: [-h] pathSyns pathSugg pathVote pathFreq trashold pathOut\
+                 \\n\tpathSyns - path to wn.json\
+                 \\n\tpathSugg - path to suggestions.json\
+                 \\n\tpathVote - path to votes.json\
+                 \\n\tpathFreq - path to frequencies\
+                 \\n\ttrashold - minimun acceptable fequency\
+                 \\n\tpathOut - path to output folder"
+        
+run [] = usage
+run [pathSyns,pathSugg,pathVote,pathFreq,trashold,pathOut] =
+  (groupBy f . sortBy g) <$> synsets >>= (mapM_ $ putToFile pathOut)
   where
-    id_filter = fmap (c0 1) id_scores
-    sg_filter = c2 <$> sg_doc <*> id_filter
-    sy_doc = fmap (f1) (readJL readSynset "/home/fredson/wn/dump/wn.json")
-    sg_doc = fmap (c1 . f1) (readJL readSuggestion "/home/fredson/wn/dump/suggestion.json")
-    id_scores = fmap (f3 . f2 . f1) (readJL readVote "/home/fredson/wn/dump/votes.json")
+    f x y = (relation x, typeA x,typeB x) == (relation y, typeA y,typeB y)
+    g x y = compare (relation x, typeA x,typeB x) (relation y, typeA y,typeB y)
+    synsets = processSynsets (read trashold :: Integer) pathSyns pathSugg pathVote pathFreq
+run inputs = usage
+
+
+putToFile :: FilePath -> [SPointer] -> IO ()
+putToFile pathOut spointers = do
+  createDirectoryIfMissing True pathOut
+  writeFile filename output 
+  where
+    first = head spointers
+    output = (intercalate "\n" $ map showOut spointers)
+    filename = pathOut++"/"++(relation first)++"-"++(typeA first)++"-"++(typeB first)++".txt"
+    showOut spointer = (wordA spointer) ++ "\t" ++ (intercalate "/" $ wordB spointer)
+
+{- runs the update task -}
+processSynsets :: Integer -> FilePath -> FilePath -> FilePath -> FilePath -> IO [SPointer]
+processSynsets trashold pathSyns pathSugg pathVote pathFreq =
+  (filter g . map fst . sortBy f) <$> filtered
+  where
+    g x = (length $ wordB x) > 0
+    f x y = compare (snd y) (snd x)
+    filtered = frequencyFilter trashold <$> (filteredSyns pathSyns pathSugg pathVote) <*> (filteredFreq pathFreq)
+
+filteredFreq pathFreq = (parseFrequencies) <$> (getFrequencies pathFreq)
+filteredSyns pathSyns pathSugg pathVote =
+  collectRelationsSenses <$> (c4 <$> synsDocs <*> suggFilter)
+  where
+    idFilter = fmap (c0 1) scoreIds
+    suggFilter = c2 <$> suggDocs <*> idFilter
+    synsDocs = fmap (f1) (readJL readSynset pathSyns)
+    suggDocs = fmap (c1 . f1) (readJL readSuggestion pathSugg)
+    scoreIds = fmap (f3 . f2 . f1) (readJL readVote pathVote)
